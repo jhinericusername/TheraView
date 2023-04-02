4dad944031cd3976b2f7cb620354238a29dccb1d
Chtun <chtun@live.com>
2023-04-02 01:54:52 -0500

    debugging

--- Framify.py
+++ Framify.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2
 
 # read the video file
@@ -28,34 +27,3 @@ print('Extracted', len(frames), 'frames from the video')
 cv2.imshow('First Frame', frames[0])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-=======
-import cv2
-
-# read the video file
-cap = cv2.VideoCapture('jj.mp4')
-
-# initialize an empty list to store frames
-frames = []
-
-# loop through all the frames in the video
-while (cap.isOpened()):
-    # read a frame from the video
-    ret, frame = cap.read()
-    # if the frame is read correctly
-    if ret == True:
-        # append the frame to the list of frames
-        frames.append(frame)
-    else:
-        break
-
-# release the video capture object
-cap.release()
-
-# print the number of frames extracted
-print('Extracted', len(frames), 'frames from the video')
-
-# display the first frame as an example
-cv2.imshow('First Frame', frames[0])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
->>>>>>> bf3604ec78f035b027b5e839b5305601cdded50f
